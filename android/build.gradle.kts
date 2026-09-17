7e6e15ce95d029ffd444cb97899c6111b4169e6a
wicolian <136704438+wicolian@users.noreply.github.com>
2026-09-17 23:55:11 +0000

    build: update android gradle plugin

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "8.7.3" apply false
-    id("com.android.library") version "8.7.3" apply false
+    id("com.android.application") version "8.9.1" apply false
+    id("com.android.library") version "8.9.1" apply false
     kotlin("android") version "2.0.21" apply false
     kotlin("jvm") version "2.0.21" apply false
     kotlin("plugin.serialization") version "2.0.21" apply false
