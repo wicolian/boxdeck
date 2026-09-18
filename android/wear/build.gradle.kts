9672ede5202f29ffd9969ce851902c291a5b2902
wicolian <136704438+wicolian@users.noreply.github.com>
2026-09-18 00:43:22 +0000

    build: migrate to agp built in kotlin

--- android/wear/build.gradle.kts
+++ android/wear/build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.android.application")
-    kotlin("android")
     kotlin("plugin.serialization")
     kotlin("plugin.compose")
 }
@@ -20,7 +19,11 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-    kotlinOptions { jvmTarget = "17" }
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
+    }
     packaging { resources.excludes += "/META-INF/{AL2.0,LGPL2.1}" }
 }
 
