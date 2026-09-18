443728816549cfe098fb528293b2b4de31d88395
wicolian <136704438+wicolian@users.noreply.github.com>
2026-09-18 00:03:29 +0000

    build: align kotlin with unifiedpush

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
     id("com.android.application") version "8.9.1" apply false
     id("com.android.library") version "8.9.1" apply false
-    kotlin("android") version "2.0.21" apply false
-    kotlin("jvm") version "2.0.21" apply false
-    kotlin("plugin.serialization") version "2.0.21" apply false
-    kotlin("plugin.compose") version "2.0.21" apply false
+    kotlin("android") version "2.2.10" apply false
+    kotlin("jvm") version "2.2.10" apply false
+    kotlin("plugin.serialization") version "2.2.10" apply false
+    kotlin("plugin.compose") version "2.2.10" apply false
 }
